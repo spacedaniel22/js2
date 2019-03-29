05bb559e65aa53f1c25aaeb16b0229d7233f2b0d
Daniel Lorincz <daniel.lorincz@ibm.com>
2019-03-29 11:41:41 +0100

    added more error handling

--- index.js
+++ index.js
@@ -5,20 +5,43 @@ console.log("First");
 console.log("Second");
 
 function later() {
-    setTimeout(() => console.log("Third???"), 3000);
+    setTimeout(() => console.log("Third???"), 1000);
 }
 
 console.log("I'M THIRD!");
 
-// const promisefn = () => new Promise((resolve, reject) => {
-//     setTimeout(() => resolve(22), 5000);
-// });
+const promisefn = () => new Promise((resolve, reject) => {
+    if ((Math.random() * 10).toFixed() > 5 ) {
+        reject("BAJ VAN");
+    }
+    setTimeout(() => resolve(22), 1000);
+});
 
-// promisefn().then((result) => console.log(result));
+promisefn()
+    .then((result) => {
+        console.log(result);
+        return result;
+    })
+    .then(result2 => console.log(result2 + 1))
+    .catch((error) => console.log(error));
 
-// const aaa = async () => {
-//     const bbb = await promisefn();
-//     return bbb+11;
-// }
+const aaa = async () => {
+    try {
+        const bbb = await promisefn();
+        return bbb + 11;
+    } catch(error) {
+        // throw new Error(error);
+        throw "Micimackó";
+    }
+}
 
-// aaa().then((result) => console.log(result));
+
+aaa()
+    .then((result) => {
+        console.log(result);
+        document.querySelector("body").innerHTML = result;
+    })
+    .catch((error) => {
+        document.title = error;
+        console.log(error);
+    });
